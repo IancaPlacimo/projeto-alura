f34000ac10f1f1ec709c50efd4153f3508a227a0
Ianca Placimo <iancaplacimo@gmail.com.br>
2023-08-08 21:25:30 -0300

    inicio do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[HeaderHomeComponent]
 })
 export class AppModule { }
